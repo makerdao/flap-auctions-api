adfdf29e03e911dfcf63cd86703672ec1189cc9f
grandizzy <grandizzy@localhost>
2020-10-28 16:43:33 +0200

    Add CORS headers

--- flap_auctions/api.py
+++ flap_auctions/api.py
@@ -22,34 +22,39 @@ class FlapAuctionsHandler(tornado.web.RequestHandler):
                 days_ago = int((current_time - datetime.timedelta(days=days_ago_param)).timestamp())
                 result = self.db.get_all_events(days_ago)
                 self.add_event_ids(result)
-                self.write(json.dumps(result))
+                self.write_result(result)
             else:
                 result = self.db.get_events(int(id))
                 if not result:
                     self.send_error(404)
                 else:
                     self.add_event_ids(result)
-                    self.write(json.dumps(result))
+                    self.write_result(result)
         else:
             status = self.get_argument("status", "", True)
             if status == "all":
                 kicks = self.db.get_all_kicks()
-                self.write(json.dumps(self.all_auction_response(kicks)))
+                self.write_result(self.all_auction_response(kicks))
             elif status == "open":
                 current_time = datetime.datetime.now()
                 ttl_minutes_ago = int((current_time - datetime.timedelta(minutes=FLAPPER_TTL_MINUTES)).timestamp())
                 kicks = self.db.get_kicks(ttl_minutes_ago, False)
-                self.write(json.dumps(self.filtered_auction_response(kicks, 'open')))
+                self.write_result(self.filtered_auction_response(kicks, 'open'))
             elif status == "closed":
                 current_time = datetime.datetime.now()
                 ttl_minutes_ago = int((current_time - datetime.timedelta(minutes=FLAPPER_TTL_MINUTES)).timestamp())
                 kicks = self.db.get_kicks(ttl_minutes_ago, True)
-                self.write(json.dumps(self.filtered_auction_response(kicks, 'closed')))
+                self.write_result(self.filtered_auction_response(kicks, 'closed'))
 
             address = self.get_argument("address", None, True)
             if address:
                 tends = self.db.get_tends(address)
-                self.write(json.dumps(tends))
+                self.write_result(tends)
+
+    def write_result(self, result):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header('Content-Type', 'application/json')
+        self.write(json.dumps(result))
 
     @staticmethod
     def all_auction_response(kicks: []):
