a5758a5adfd5e6139cad9c5643ff82223aef795f
grandizzy <grandizzy@localhost>
2020-10-30 15:37:48 +0200

    Allow Metamask to connect to local testchain

--- tests/start_testchain.sh
+++ tests/start_testchain.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -p 8545:8545 -u="root" -w="/home/parity" --name="testchain" makerdao/testchain-pymaker:unit-testing-1.1.0
+docker run --rm -p 8545:8545 -p 8546:8546 -u="root" -w="/home/parity" --name="testchain" --entrypoint="/bin/sh" makerdao/testchain-pymaker:unit-testing-1.1.0 -c "/bin/parity --chain=/home/parity/config/parity-dev-constantinopole.json --tracing=on --pruning=archive --jsonrpc-interface=all --jsonrpc-hosts=all --jsonrpc-cors=all --base-path=/home/parity/.local/share/io.parity.ethereum/"
